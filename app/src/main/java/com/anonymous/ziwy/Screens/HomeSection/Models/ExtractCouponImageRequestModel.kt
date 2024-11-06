f162da383d12eada2b7253430a76330d4a08f3b0
tanmayshree <tanmay.25ts@gmail.com>
2024-11-06 16:11:23 +0530

    model updated

---
+++ app/src/main/java/com/anonymous/ziwy/Screens/HomeSection/Models/ExtractCouponImageRequestModel.kt
@@ -0,0 +1,8 @@
+package com.anonymous.ziwy.Screens.HomeSection.Models
+
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class ExtractCouponImageRequestModel(
+    val base64: String? = null
+)
