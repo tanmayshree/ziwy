565229abd9dcb8be14a673618b45ed02c85dcb95
tanmayshree <tanmay.25ts@gmail.com>
2024-11-12 20:50:29 +0530

    new version change

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -26,8 +26,8 @@ android {
         applicationId = "com.anonymous.ziwy"
         minSdk = 24
         targetSdk = 34
-        versionCode = 20000
-        versionName = "2.0.0"
+        versionCode = 20001
+        versionName = "2.0.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
